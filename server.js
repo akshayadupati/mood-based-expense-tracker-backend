b93522916fa8082190b772eacf08c27980d3d618
Akshaya Dupati <98553741+akshayadupati@users.noreply.github.com>
2022-10-11 18:31:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const mongoose = require("mongoose");
 require("dotenv").config({ path: "./config.env" });
 
 const port = process.env.PORT || 5000;
@@ -9,7 +9,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const connection = require("./db/connection");
+const connection = mongoose
+  .connect(process.env.ATLAS_URI, { useNewUrlParser: true })
+  .then((db) => {
+    console.log("Database connected!");
+    return db;
+  })
+  .catch((err) => {
+    console.log("Connection error", err);
+  });
 
 app.use(require("./routes/route"));
 
